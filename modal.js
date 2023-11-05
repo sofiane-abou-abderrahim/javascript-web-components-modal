ea2484f65776e1eb5381fc24c787bd75706430c4
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-11-05 22:06:01 +0200

    set up the general template by adding a shadow dom + added some style to the backdrop

--- modal.js
+++ modal.js
@@ -1,6 +1,21 @@
 class Modal extends HTMLElement {
   constructor() {
     super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+      <style>
+        #backdrop {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100vh;
+          background: rgba(0,0,0,0.75);
+        }
+      </style>
+      <div id="backdrop"></div>
+      <div id="modal"></div>
+    `;
   }
 }
 
