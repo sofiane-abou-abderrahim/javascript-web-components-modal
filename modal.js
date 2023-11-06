40f265a07de4b062d74406050f7f8263c2d42299
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-11-06 11:43:03 +0200

    corrected error on slots constant and slot array indec

--- modal.js
+++ modal.js
@@ -80,10 +80,10 @@ class Modal extends HTMLElement {
         </section>
       </div>
     `;
-    const slots = document.querySelectorAll('slot');
-    slots.[1].addEventListener('slotchange', event => {
+    const slots = this.shadowRoot.querySelectorAll('slot');
+    slots[1].addEventListener('slotchange', event => {
       console.dir(slots[1]);
-    })
+    });
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
