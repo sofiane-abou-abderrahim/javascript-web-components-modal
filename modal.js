2d4edd5eb56c9235f426c238b6ecf03440df5020
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-11-06 11:30:03 +0200

    added named slot for h1 and set up a callback content

--- modal.js
+++ modal.js
@@ -69,7 +69,7 @@ class Modal extends HTMLElement {
       <div id="backdrop"></div>
       <div id="modal">
         <header>
-          <h1>Please Confirm</h1>
+          <slot name="title">Please Confirm Payment</slot>
         </header>
         <section id="main">
           <slot></slot>
